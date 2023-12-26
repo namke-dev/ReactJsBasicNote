267ffb79935f8dab534e4effaf4347bf66ae94aa
namke-dev <namke.dev@gmail.com>
2023-12-26 14:50:38 +0700

    handle multiple input field using state

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react'
-
 const Form = () => {
-    function handleChange(e){
-        // console.log(e.target.value)
-        setTextFieldValue(e.target.value)
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(getName);
     }
-    const [textFieldValue, setTextFieldValue] = useState("");
+    const [getName, setName] = useState({firstName:"", lastName:""});
     return (
     <div>
+        {getName.firstName} - {getName.lastName}
         <form>
             <input 
-                onChange={ (e) => handleChange(e)}
+                onChange={ (e) => setName({...getName, firstName: e.target.value})}
                 type='text' 
-                placeholder='Input your name here' 
-                value={textFieldValue}>
+                placeholder='First name' 
+                value={getName.firstName}>
             </input>
-            <button>Submit</button>
+            <input 
+                onChange={ (e) => setName({...getName, lastName : e.target.value})}
+                type='text' 
+                placeholder='Last name' 
+                value={getName.lastName}>
+            </input>
+            <button onClick={(e) => handleSubmit(e)}>Submit</button>
         </form>
     </div>
   )
